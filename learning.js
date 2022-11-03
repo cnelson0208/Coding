9ede9b93b34068876c4e7df089e6c1f81a2a60c3
Chance Nelosn <Cnelson0208@gmail.com>
2022-11-03 10:02:32 -0400

    test

---
+++ learning.js
@@ -0,0 +1,13 @@
+console.log("Hello World!"); 
+let sentence = ("Hello World!")
+console.log (sentence);
+console.log (sentence + "Hello World!");
+
+let name = ("Chance");
+console.log(name);
+console.log (name + "Chance" );
+
+
+console.log("843-901-3543");
+console.log(843-901-3543); 
+
