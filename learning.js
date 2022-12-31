c78aacb9f9fea4e3b2132aefa6127ff4f611ed3c
Chance Nelosn <Cnelson0208@gmail.com>
2022-12-31 12:15:37 -0500

    test

--- learning.js
+++ learning.js
@@ -104,3 +104,8 @@ for (let index = 1; index <= 5; index++) {
     console.log(`${index} is even`);
     }
     } 
+
+    function sayHello(Chance, Nelson) {
+        const message = `Hello, ${Chance} ${Nelson}!`;
+        return message; 
+        } 
