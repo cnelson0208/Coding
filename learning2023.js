d1d446f7d88fa5b9bf7e12141afc1ddf15bffc91
Chance Nelosn <Cnelson0208@gmail.com>
2023-01-12 10:03:03 -0500

    test

--- learning2023.js
+++ learning2023.js
@@ -1,5 +1,17 @@
-console.log(Math.random() * 5);
+const cl = console.log;
+
+cl(Math.round(5) * 1000);
 
 let d = new Date 
-console.log(new Date("1/1/2023"));
+cl(new Date("1/1/2023"));
 
+cl(new Date("2/8/2007"));
+
+function randomNumber(min, max) { 
+    return Math.floor(Math.random() * (max - min) + min);
+}  
+cl( randomNumber(1, 1000) );
+
+let myGames = ['Arma', 'Insurgency'];
+let games = myGames[Math.floor(Math.random() * myGames.length)];
+cl(games);
