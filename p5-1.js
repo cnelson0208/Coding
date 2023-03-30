fa1b0d0a25d1843e78d3bb20a8a3607cbd21f64c
Chance Nelosn <Cnelson0208@gmail.com>
2023-03-30 10:04:29 -0400

    test

--- p5-1.js
+++ p5-1.js
@@ -1,9 +1,16 @@
 /// <reference path="lib/p5.global.d.ts" />
 
 var setup = function () {
-  createCanvas(600, 400);
+  createCanvas(2000, 800);
   frameRate(5);
 };
 var draw = function () {
   background(200);
+
+  for (let y = 0; y < 2000; y += 60) {
+    for (let x = 0; x < 2000; x += 60) {
+      fill(color(50, 100, 150, 210));
+      circle(x, y, random(50, 159, 255));
+    }
+  }
 };
